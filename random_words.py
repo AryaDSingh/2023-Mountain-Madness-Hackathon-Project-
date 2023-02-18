2f6d59f66eb86be917a1390f88c368fb2a44857a
Adam Avdic <adamavdic@gmail.com>
2023-02-18 11:17:08 -0800

    pushing words

---
+++ random_words.py
@@ -0,0 +1,13 @@
+# Python code to pick a random
+# word from a text file
+import random
+  
+# Open the file in read mode
+with open("MountainWords.txt", "r") as file:
+    allText = file.read()
+    words = list(map(str, allText.split()))
+  
+    # print random string
+   # print(random.choice(words))
+
+print(I love )
